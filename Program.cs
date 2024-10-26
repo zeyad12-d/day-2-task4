09b9a0f071d53e4e279e85de79fdbdde62aed131
zeyad12-d <zeadyasser054@gmail.com>
2024-10-26 07:15:17 +0300

    task4

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.Design;
+
+namespace day2_task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(  "Enter Number Between, 2 , and , 9 ,");
+            int num=int.Parse( Console.ReadLine() );
+
+            for (int i = 0; i < num; i++) 
+            {
+                int ruelt = 0;
+                if (num >= 2 && num <= 9)
+
+                {
+                    ruelt= num * i;
+                    Console.WriteLine($"the num is {num} * {i} = {ruelt} ");
+                }
+
+                else
+                {
+                    Console.WriteLine(" is out of rang");
+                }
+            }
+
+
+            
+        }
+    }
+}
